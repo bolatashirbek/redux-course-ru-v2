94e7207d31d683750f6baf718e38b4fec2cac74c
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-05-26 19:09:54 +0600

    Сhanged the data in the store and displayed it in the component using connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import './App.css'
+import '../App.css'
 
 class App extends Component {
   render() {
+    const { name, surname, age } = this.props.user
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p className="App-intro">Здесь будут мои самые залайканные фото</p>
-        <p>Меня зовут: {this.props.user}</p>
+        <p className="App-intro">
+          Привет из App, {name} {surname}!
+        </p>
+        <p>Тебе уже {age}?</p>
       </div>
     )
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 export const initialState = {
-  user: 'Unknown user',
+  user: {
+    name: 'Василий',
+    surname: 'Реактов',
+    age: 27,
+  },
 }
 
 export function rootReducer(state = initialState) {
