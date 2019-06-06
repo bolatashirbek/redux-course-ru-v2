459d9858cf7d008fd55c6635f58ca6498582cc98
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-06-07 00:07:23 +0600

    Added a few buttons over the years and a click handler for them

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,10 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export class Page extends React.Component {
+  onBtnClick = e => {
+    const year = +e.currentTarget.innerText
+    this.props.setYear(year)
+  }
+
   render() {
     const { year, photos } = this.props
     return (
       <div>
+        <div>
+          <button onClick={this.onBtnClick}>2019</button>
+          <button onClick={this.onBtnClick}>2018</button>
+          <button onClick={this.onBtnClick}>2017</button>
+          <button onClick={this.onBtnClick}>2016</button>
+          <button onClick={this.onBtnClick}>2015</button>
+        </div>
         <p>
           У тебя {photos.length} фото за {year} год
         </p>
@@ -17,4 +29,5 @@ export class Page extends React.Component {
 Page.propTypes = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
+  setYear: PropTypes.func.isRequired,
 }
